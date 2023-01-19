96a33a408e489f2b7077e8d1e66fa5720b8f316f
Matt <39172048+magiccreb@users.noreply.github.com>
2023-01-18 23:21:56 -0500

    Added preliminary app.use stuff in commentary and tested port functionality.

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// work to come.
+// Initialization/node stuff
+const express = require("express");
+// const path = require("path");
+// const fs = require("fs");
+
+const app = express();
+const PORT = 6505;
+
+/*
+// Setup for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+*/
+
+// app.use(express.static("public"));
+
+// Routes
+
+// Loads the homepage
+app.get("/", (req, res) => res.send("Just seeing if we're working here."));
+
+// Listener
+app.listen(PORT, () =>
+    console.log(`Note-taker is listening at http://localhost:${PORT}`)
+);
